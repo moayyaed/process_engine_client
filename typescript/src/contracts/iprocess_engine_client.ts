63096162b3115e9656ad3c2bda6ca623a28d7c89
ElRaptorus <christian.werner@5minds.de>
2019-07-18 10:20:19 +0200

    :fire: Remove superfluous notifications from IProcessEngineClient The removed notifications resemble very specific use cases, the responsibility of which does not lie with the ProcessEngine, but rather the user that requires them.

--- typescript/src/contracts/iprocess_engine_client.ts
+++ typescript/src/contracts/iprocess_engine_client.ts
@@ -106,27 +106,14 @@ export interface IProcessEngineClient {
     timeout?: number,
   ): ExternalTaskWorker;
 
-  // Notifications
-  onActivityReached(callback: Messages.CallbackTypes.OnActivityReachedCallback, subscribeOnce?: boolean): Promise<Subscription>;
-
-  onActivityFinished(callback: Messages.CallbackTypes.OnActivityFinishedCallback, subscribeOnce?: boolean): Promise<Subscription>;
-
-  // ------------ For backwards compatibility only
-  onCallActivityWaiting(callback: Messages.CallbackTypes.OnCallActivityWaitingCallback, subscribeOnce?: boolean): Promise<Subscription>;
-
-  onCallActivityFinished(callback: Messages.CallbackTypes.OnCallActivityFinishedCallback, subscribeOnce?: boolean): Promise<Subscription>;
-  // ------------
-
+  // Socket.IO Notifications
   onEmptyActivityWaiting(callback: Messages.CallbackTypes.OnEmptyActivityWaitingCallback, subscribeOnce?: boolean): Promise<Subscription>;
 
   onEmptyActivityFinished(callback: Messages.CallbackTypes.OnEmptyActivityFinishedCallback, subscribeOnce?: boolean): Promise<Subscription>;
 
-  onEmptyActivityForIdentityWaiting(callback: Messages.CallbackTypes.OnEmptyActivityWaitingCallback, subscribeOnce?: boolean): Promise<Subscription>;
+  onManualTaskWaiting(callback: Messages.CallbackTypes.OnManualTaskWaitingCallback, subscribeOnce?: boolean): Promise<Subscription>;
 
-  onEmptyActivityForIdentityFinished(
-    callback: Messages.CallbackTypes.OnEmptyActivityFinishedCallback,
-    subscribeOnce?: boolean,
-  ): Promise<Subscription>;
+  onManualTaskFinished(callback: Messages.CallbackTypes.OnManualTaskFinishedCallback, subscribeOnce?: boolean): Promise<Subscription>;
 
   onUserTaskWaiting(callback: Messages.CallbackTypes.OnUserTaskWaitingCallback, subscribeOnce?: boolean): Promise<Subscription>;
 
@@ -149,31 +136,17 @@ export interface IProcessEngineClient {
     subscribeOnce?: boolean,
   ): Promise<Subscription>;
 
-  onUserTaskForIdentityWaiting(callback: Messages.CallbackTypes.OnUserTaskWaitingCallback, subscribeOnce?: boolean): Promise<Subscription>;
+  onNonInteractiveActivityReached(callback: Messages.CallbackTypes.OnActivityReachedCallback, subscribeOnce?: boolean): Promise<Subscription>;
 
-  onUserTaskForIdentityFinished(callback: Messages.CallbackTypes.OnUserTaskFinishedCallback, subscribeOnce?: boolean): Promise<Subscription>;
+  onNonInteractiveActivityFinished(callback: Messages.CallbackTypes.OnActivityFinishedCallback, subscribeOnce?: boolean): Promise<Subscription>;
+
+  onProcessStarted(callback: Messages.CallbackTypes.OnProcessStartedCallback, subscribeOnce?: boolean): Promise<Subscription>;
+
+  onProcessEnded(callback: Messages.CallbackTypes.OnProcessEndedCallback, subscribeOnce?: boolean): Promise<Subscription>;
 
   onProcessTerminated(callback: Messages.CallbackTypes.OnProcessTerminatedCallback, subscribeOnce?: boolean): Promise<Subscription>;
 
   onProcessError(callback: Messages.CallbackTypes.OnProcessErrorCallback, subscribeOnce?: boolean): Promise<Subscription>;
-
-  onProcessStarted(callback: Messages.CallbackTypes.OnProcessStartedCallback, subscribeOnce?: boolean): Promise<Subscription>;
-
-  onProcessWithProcessModelIdStarted(
-    callback: Messages.CallbackTypes.OnProcessStartedCallback,
-    processModelId: string,
-    subscribeOnce?: boolean,
-  ): Promise<Subscription>;
-
-  onManualTaskWaiting(callback: Messages.CallbackTypes.OnManualTaskWaitingCallback, subscribeOnce?: boolean): Promise<Subscription>;
-
-  onManualTaskFinished(callback: Messages.CallbackTypes.OnManualTaskFinishedCallback, subscribeOnce?: boolean): Promise<Subscription>;
-
-  onManualTaskForIdentityWaiting(callback: Messages.CallbackTypes.OnManualTaskWaitingCallback, subscribeOnce?: boolean): Promise<Subscription>;
-
-  onManualTaskForIdentityFinished(callback: Messages.CallbackTypes.OnManualTaskFinishedCallback, subscribeOnce?: boolean): Promise<Subscription>;
-
-  onProcessEnded(callback: Messages.CallbackTypes.OnProcessEndedCallback, subscribeOnce?: boolean): Promise<Subscription>;
 
   removeSubscription(subscription: Subscription): Promise<void>;
 
