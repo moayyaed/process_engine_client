4ea8721d7ce9ab774db419a54f0cc67f62d8f00b
ElRaptorus <christian.werner@5minds.de>
2019-07-26 13:59:07 +0200

    :white_check_mark: Add .NET tests for EmptyActivities

---
+++ dotnet/tests/ProcessEngineClient/ProcessEngineBaseTest.cs
@@ -0,0 +1,22 @@
+namespace ProcessEngine.Client.Tests
+{
+  using System;
+
+  public class ProcessEngineBaseTest
+    {
+        private string processEngineRestApiUrl;
+
+        public ProcessEngineBaseTest()
+        {
+            SetProcessEngineRestApiUrl();
+        }
+
+        private void SetProcessEngineRestApiUrl()
+        {
+            var baseUrlFromEnv = Environment.GetEnvironmentVariable("PROCESS_ENGINE_REST_API_URL");
+            var baseUrl = string.IsNullOrEmpty(baseUrlFromEnv) ? "http://localhost:8080" : baseUrlFromEnv;
+
+            this.processEngineRestApiUrl = $"{baseUrl}";
+        }
+    }
+}
