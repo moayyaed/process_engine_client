b2e63004ebbe4060cc4665a8661e4416fc136b9e
ElRaptorus <christian.werner@5minds.de>
2019-07-26 09:16:58 +0200

    :recycle: Use Arrays as return types on .TS Client Aspects

--- typescript/src/contracts/interfaces/client_aspects/iempty_activity_client.ts
+++ typescript/src/contracts/interfaces/client_aspects/iempty_activity_client.ts
@@ -2,18 +2,18 @@ import {DataModels} from '@process-engine/consumer_api_contracts';
 
 export interface IEmptyActivityClient {
 
-  getSuspendedEmptyActivitiesForProcessModel(processModelId: string): Promise<DataModels.EmptyActivities.EmptyActivityList>;
+  getSuspendedEmptyActivitiesForProcessModel(processModelId: string): Promise<Array<DataModels.EmptyActivities.EmptyActivity>>;
 
-  getSuspendedEmptyActivitiesForProcessInstance(processInstanceId: string): Promise<DataModels.EmptyActivities.EmptyActivityList>;
+  getSuspendedEmptyActivitiesForProcessInstance(processInstanceId: string): Promise<Array<DataModels.EmptyActivities.EmptyActivity>>;
 
-  getSuspendedEmptyActivitiesForCorrelation(correlationId: string): Promise<DataModels.EmptyActivities.EmptyActivityList>;
+  getSuspendedEmptyActivitiesForCorrelation(correlationId: string): Promise<Array<DataModels.EmptyActivities.EmptyActivity>>;
 
   getSuspendedEmptyActivitiesForProcessModelInCorrelation(
     processModelId: string,
     correlationId: string,
-  ): Promise<DataModels.EmptyActivities.EmptyActivityList>;
+  ): Promise<Array<DataModels.EmptyActivities.EmptyActivity>>;
 
-  getSuspendedEmptyActivitiesForClientIdentity(): Promise<DataModels.EmptyActivities.EmptyActivityList>;
+  getSuspendedEmptyActivitiesForClientIdentity(): Promise<Array<DataModels.EmptyActivities.EmptyActivity>>;
 
   finishEmptyActivity(
     processInstanceId: string,
