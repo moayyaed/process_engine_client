8c1f1dad6d0d430971927e13d5d217491fc983a3
ElRaptorus <christian.werner@5minds.de>
2019-07-26 10:57:27 +0200

    .recycle: Remove .NET Http handling to HttpFacade

---
+++ dotnet/src/HttpFacade/HttpFacade.cs
@@ -0,0 +1,163 @@
+namespace ProcessEngine.Client
+{
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+
+    using ProcessEngine.ConsumerAPI.Contracts.DataModel;
+
+    using ConsumerApiRestSettings = ProcessEngine.ConsumerAPI.Contracts.RestSettings;
+
+    using EssentialProjects.IAM.Contracts;
+
+    internal class HttpFacade
+    {
+        private HttpClient HttpClient { get; }
+
+        private IIdentity Identity { get; set; }
+
+        public HttpFacade(string url, IIdentity identity)
+        {
+            this.HttpClient = new HttpClient();
+            this.HttpClient.BaseAddress = new Uri(url);
+            this.Identity = identity;
+        }
+
+        public string EndpointAddress {
+            get {
+                return this.HttpClient.BaseAddress.ToString();
+            }
+        }
+
+        public async Task<ProcessModel> GetProcessModelFromUrl(string url)
+        {
+            var result = await this.SendRequestAndExpectResult<ProcessModel>(HttpMethod.Get, url);
+
+            return result;
+        }
+
+        public async Task<EventList> GetTriggerableEventsFromUrl(string url)
+        {
+            var result = await this.SendRequestAndExpectResult<EventList>(HttpMethod.Get, url);
+
+            return result;
+        }
+
+        public async Task<EmptyActivityList> GetEmptyActivitiesFromUrl(string url)
+        {
+            var result = await this.SendRequestAndExpectResult<EmptyActivityList>(HttpMethod.Get, url);
+
+            return result;
+        }
+
+        public async Task<ManualTaskList> GetManualTasksFromUrl(string url)
+        {
+            var result = await this.SendRequestAndExpectResult<ManualTaskList>(HttpMethod.Get, url);
+
+            return result;
+        }
+
+        public async Task<UserTaskList> GetUserTasksFromUrl(string url)
+        {
+            var result = await this.SendRequestAndExpectResult<UserTaskList>(HttpMethod.Get, url);
+
+            return result;
+        }
+
+        public async Task SendRequestAndExpectNoResult<TRequest>(HttpMethod method, string endpoint, TRequest content = default(TRequest))
+        {
+            var url = this.ApplyBaseConsumerApiUrl(endpoint);
+
+            var request = this.CreateRequestMessage<TRequest>(this.Identity, method, url, content);
+            var result = await this.HttpClient.SendAsync(request);
+
+            if (!result.IsSuccessStatusCode)
+            {
+                throw new Exception($"Request failed: {result.ReasonPhrase}");
+            }
+        }
+
+        public async Task<TResult> SendRequestAndExpectResult<TResult>(HttpMethod method, string endpoint)
+        {
+            return await this.SendRequestAndExpectResult<object, TResult>(method, endpoint, null);
+        }
+
+        public async Task<TResult> SendRequestAndExpectResult<TRequest, TResult>(HttpMethod method, string endpoint, TRequest payload)
+        {
+            var url = this.ApplyBaseConsumerApiUrl(endpoint);
+
+            TResult parsedResult = default(TResult);
+
+            var request = this.CreateRequestMessage<TRequest>(this.Identity, method, url, payload);
+            var result = await this.HttpClient.SendAsync(request);
+
+            if (result.IsSuccessStatusCode)
+            {
+                var jsonResult = await result.Content.ReadAsStringAsync();
+                parsedResult = JsonConvert.DeserializeObject<TResult>(jsonResult);
+            }
+            else
+            {
+                throw new Exception($"Request failed: {result.ReasonPhrase}");
+            }
+
+            return parsedResult;
+        }
+
+        public HttpRequestMessage CreateRequestMessage<TRequest>(IIdentity identity, HttpMethod method, string url, TRequest content = default(TRequest))
+        {
+            var hasNoIdentity = identity == null || identity.Token == null;
+            if (hasNoIdentity)
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            var message = new HttpRequestMessage();
+
+            message.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", identity.Token);
+
+            message.RequestUri = new Uri(this.HttpClient.BaseAddress, url);
+            message.Method = method;
+            message.Content = content == null
+                ? null
+                : this.SerializePayload<TRequest>(content);
+
+            return message;
+        }
+
+        public string ApplyBaseConsumerApiUrl(string url)
+        {
+            return $"${ConsumerApiRestSettings.Endpoints.ConsumerAPI}${url}";
+        }
+
+        public void SetAuthenticationHeader(IIdentity identity)
+        {
+            this.HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", identity.Token);
+        }
+
+        public StringContent SerializePayload<TRequest>(TRequest request)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+
+            var serializedRequest = JsonConvert.SerializeObject(request, settings);
+            var content = new StringContent(serializedRequest, Encoding.UTF8, "application/json");
+            return content;
+        }
+
+        public async Task<TResponse> DeserializeResposne<TResponse>(HttpResponseMessage response)
+        {
+            var serializedResponse = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<TResponse>(serializedResponse);
+        }
+    }
+}
--- dotnet/src/ProcessEngineClient.cs
+++ dotnet/src/ProcessEngineClient.cs
@@ -2,29 +2,24 @@ namespace ProcessEngine.Client
 {
     using System;
     using System.Net.Http;
-    using System.Net.Http.Headers;
-    using System.Text;
     using System.Threading.Tasks;
-
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Serialization;
 
     using ProcessEngine.ConsumerAPI.Contracts.DataModel;
 
-    using ConsumerApiRestSettings = ProcessEngine.ConsumerAPI.Contracts.RestSettings;
-
     using EssentialProjects.IAM.Contracts;
+
+    using ConsumerApiRestSettings = ProcessEngine.ConsumerAPI.Contracts.RestSettings;
 
     using ProcessEngine.Client.Contracts;
 
-    public class ProcessEngineClient
+    public class ProcessEngineClient: IProcessEngineClient
     {
         private static IIdentity DummyIdentity = new Identity() {
             UserId = "ZHVtbXlfdG9rZW4=",
             Token = "dummy_token"
         };
 
-        private HttpClient HttpClient { get; }
+        private HttpFacade HttpFacade { get; }
 
         private IIdentity Identity { get; set; }
 
@@ -35,8 +30,7 @@ namespace ProcessEngine.Client
 
         public ProcessEngineClient(string url, IIdentity identity)
         {
-            this.HttpClient = new HttpClient();
-            this.HttpClient.BaseAddress = new Uri(url);
+            this.HttpFacade = new HttpFacade(url, identity);
             this.Identity = identity;
         }
 
@@ -62,18 +56,22 @@ namespace ProcessEngine.Client
         where TRequestPayload : new()
         where TResponsePayload : new()
         {
+            var noEndEventIdProvided = startCallbackType == StartCallbackType.CallbackOnEndEventReached &&
+                String.IsNullOrEmpty(endEventId);
+
+            if (noEndEventIdProvided)
+            {
+                throw new ArgumentNullException(nameof(endEventId), "Must provide an EndEventId, when using callback type 'CallbackOnEndEventReached'!");
+            }
+
             var payload = new ProcessStartRequestPayload<TRequestPayload>();
             payload.CallerId = request.ParentProcessInstanceId;
             payload.CorrelationId = request.CorrelationId;
             payload.InputValues = request.Payload;
 
-            var response = await this.SendProcessStartRequest<TRequestPayload>(
-                processModelId,
-                startEventId,
-                endEventId,
-                startCallbackType,
-                payload
-            );
+            var url = this.BuildStartProcessInstanceUrl(processModelId, startEventId, endEventId, startCallbackType);
+
+            var response = await this.HttpFacade.SendRequestAndExpectResult<ProcessStartRequestPayload<TRequestPayload>, ProcessStartResponsePayload>(HttpMethod.Post, url, payload);
 
             var parsedResponse = new ProcessStartResponse<TResponsePayload>(
                 response.ProcessInstanceId,
@@ -107,7 +105,7 @@ namespace ProcessEngine.Client
         where TPayload : new()
         where TResult : new()
         {
-            var externalTaskHttpClient = new ExternalTaskHttpClient(this.HttpClient.BaseAddress.ToString());
+            var externalTaskHttpClient = new ExternalTaskHttpClient(this.HttpFacade.EndpointAddress);
             var externalTaskWorker = new ExternalTaskWorker(externalTaskHttpClient);
 
             // We must not await this, because this method runs in an infinite loop that never gets resolved until the "stop" command is given.
@@ -116,74 +114,24 @@ namespace ProcessEngine.Client
             return externalTaskWorker;
         }
 
-        private async Task<ProcessStartResponsePayload> SendProcessStartRequest<TRequestPayload>(
-            string processModelId,
-            string startEventId,
-            string endEventId,
-            StartCallbackType startCallbackType,
-            ProcessStartRequestPayload<TRequestPayload> payload
-        )
-        where TRequestPayload : new()
-        {
-            var url = this.BuildStartProcessInstanceUrl(processModelId, startEventId, endEventId, startCallbackType);
-            SetAuthenticationHeader(this.Identity);
-
-            var serializedPayload = this.SerializePayload(payload);
-
-            var response = await this.HttpClient.PostAsync(url, serializedPayload);
-
-            return await DeserializeResposne<ProcessStartResponsePayload>(response);
-        }
-
         private string BuildStartProcessInstanceUrl(string processModelId, string startEventId, string endEventId, StartCallbackType startCallbackType)
         {
             var endpoint = ConsumerApiRestSettings.Paths.StartProcessInstance
                 .Replace(ConsumerApiRestSettings.Params.ProcessModelId, processModelId);
 
-            var endpointWithParameters = $"${endpoint}?start_callback_type=${startCallbackType}";
+            var url = $"${endpoint}?start_callback_type=${startCallbackType}";
 
             if (!String.IsNullOrEmpty(startEventId))
             {
-                endpointWithParameters = $"${endpointWithParameters}&start_event_id=${startEventId}";
+                url = $"${url}&start_event_id=${startEventId}";
             }
 
             if (startCallbackType == StartCallbackType.CallbackOnEndEventReached)
             {
-                endpointWithParameters = $"${endpointWithParameters}&end_event_id=${endEventId}";
+                url = $"${url}&end_event_id=${endEventId}";
             }
 
-            var finalUrl = this.ApplyBaseConsumerApiUrl(endpointWithParameters);
-
-            return finalUrl;
-        }
-
-        private string ApplyBaseConsumerApiUrl(string url)
-        {
-            return $"${ConsumerApiRestSettings.Endpoints.ConsumerAPI}${url}";
-        }
-
-        private void SetAuthenticationHeader(IIdentity identity)
-        {
-            this.HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", identity.Token);
-        }
-
-        private StringContent SerializePayload<TRequest>(TRequest request)
-        {
-            var settings = new JsonSerializerSettings
-            {
-                ContractResolver = new CamelCasePropertyNamesContractResolver()
-            };
-
-            var serializedRequest = JsonConvert.SerializeObject(request, settings);
-            var content = new StringContent(serializedRequest, Encoding.UTF8, "application/json");
-            return content;
-        }
-
-        private async Task<TResponse> DeserializeResposne<TResponse>(HttpResponseMessage response)
-        {
-            var serializedResponse = await response.Content.ReadAsStringAsync();
-
-            return JsonConvert.DeserializeObject<TResponse>(serializedResponse);
+            return url;
         }
     }
 }
