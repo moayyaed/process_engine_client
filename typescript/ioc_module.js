373220fe4f38a87522488b30dbb081e2cd5a1e56
ElRaptorus <christian.werner@5minds.de>
2019-09-30 12:42:58 +0200

    :recycle: Update ioc dependencies for internal .ts client

--- typescript/ioc_module.js
+++ typescript/ioc_module.js
@@ -10,7 +10,16 @@ function registerInContainer(container) {
   container.register('ProcessEngineHttpClient', ProcessEngineHttpClient);
 
   container.register('ProcessEngineInternalClient', ProcessEngineInternalClient)
-    .dependencies('ConsumerApiService', 'ExternalTaskApiService');
+    .dependencies(
+      'ConsumerApiEmptyActivityService',
+      'ConsumerApiEventService',
+      'ConsumerApiExternalTaskService',
+      'ConsumerApiManualTaskService',
+      'ConsumerApiNotificationService',
+      'ConsumerApiProcessModelService',
+      'ConsumerApiUserTaskService',
+      'ConsumerApiFlowNodeInstanceService',
+    );
 }
 
 module.exports.registerInContainer = registerInContainer;
